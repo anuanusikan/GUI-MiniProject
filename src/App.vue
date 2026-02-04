7928c96341ef07a042cd08a51a841e739f65e59c
anuanusikan <anuanu010426@gmail.com>
2026-02-04 22:50:02 +0530

    Initial Vue 3 SPA setup with Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-4xl font-bold text-red-500">Test</h1>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './style.pcss'
+import App from './App.vue'
+
+createApp(App).mount('#app')
